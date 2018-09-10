ae28675bcb58aa0db1f10bd83af5c723fb4dbb08
Muhammed Thanish <mnmtanish@gmail.com>
2018-09-10 11:49:32 +0530

    Fix 'this' is undefined error

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,21 @@
  * Future
  * Future extends Promise and exposes the resolve and reject methods.
  */
-export class Future<T> extends Promise<T> {
-  public resolve!: (val: T) => void;
-  public reject!: (err: Error) => void;
+export interface Future<T> extends Promise<T> {
+  resolve: (val: T) => void;
+  reject: (err: Error) => void;
+}
 
-  constructor() {
-    super((resolve, reject) => {
-      this.resolve = resolve;
-      this.reject = reject;
-    });
-  }
+/**
+ * create
+ * create creates a new Future by creating and modifying a promise instance.
+ */
+export function create<T>(): Future<T> {
+  let res: any;
+  let rej: any;
+  const promise = new Promise<T>(( _res, _rej ) => {
+    res = _res;
+    rej = _rej;
+  });
+  return Object.assign( promise, { resolve: res, reject: rej });
 }
